07411947c559d8860b7ce323e97a8496e7ab07bf
Mohhaliim <mohamed.abd-elhalim@hotmail.com>
2024-12-26 13:36:29 +0200

    removed comments from app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -27,23 +27,3 @@ function App() {
 }
 
 export default App
-
-
-/*
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyADhzUNqJW_CryBey7Yhfx4iionN5Ghfj8",
-  authDomain: "symstax-a017a.firebaseapp.com",
-  projectId: "symstax-a017a",
-  storageBucket: "symstax-a017a.firebasestorage.app",
-  messagingSenderId: "94693017291",
-  appId: "1:94693017291:web:30a98a36a5bd12cd26bd4e"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); */
